e1d3fb3854fc1afda5df2e19e75c9245ca39f7e3
kinzafatim <kinzaf54@gmail.com>
2024-07-29 12:54:03 +0500

    FP

---
+++ src/main/scala/assignments/Exercise2.scala
@@ -0,0 +1,25 @@
+// Implement isSorted, which checks whether an Array[A] is sorted according to a
+// given comparison function:
+// def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean
+
+import scala.::
+import scala.util.Random
+
+object Exercise2 {
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    val n = as.length
+    for (i <- 0 until n - 1) {
+      for (j <- 0 until n - i - 1) {
+        if (!ordered(as(j), as(j + 1))) {
+          return false
+        }
+      }
+    }
+    true
+  }
+
+  def main(args: Array[String]): Unit = {
+    val numbers = Array(5, 3, 4, 15, 6)
+    println(isSorted(numbers, (a: Int, b: Int) => a <= b))
+  }
+}
